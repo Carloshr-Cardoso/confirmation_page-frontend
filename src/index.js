bb045a2d6b01e4596a734ef4023e6872a9fe8916
CarloshrCardoso <carloshr.cardoso@outlook.com>
2020-09-14 15:18:40 -0300

    Adicionando UI-KIT

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-import './main.scss';
+import './styles/main.scss';
 
 
 ReactDOM.render(<App/>, document.getElementById('root'));
