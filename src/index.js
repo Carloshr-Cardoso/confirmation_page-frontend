129037d25a259ceaa2180119c03222e791011427
CarloshrCardoso <carloshr.cardoso@outlook.com>
2020-09-14 15:11:06 -0300

    Adicionando sass

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './main.scss';
 
 
 ReactDOM.render(<App/>, document.getElementById('root'));
